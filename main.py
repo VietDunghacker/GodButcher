b4846de141981100b539902a9aaee1fc546bba85
Dunghacker <trungdunghoang92@gmail.com>
2020-04-27 17:13:20 +0700

    Update 7

--- main.py
+++ main.py
@@ -5,43 +5,14 @@ import re
 import string
 import os
 import sys
+import utilities
 from urllib import request
 from nltk import FreqDist
-from nltk.corpus import stopwords 
 from nltk.stem import PorterStemmer 
 from nltk.tokenize import word_tokenize
 from nltk.util import bigrams
 
-#collect bad words
-bad_words = {}
-
-for file in os.listdir("./bad_word/csv_file/"):
-	with open("./bad_word/csv_file/" + file, errors = 'ignore') as f:
-		data = csv.reader(f, delimiter = ";")
-		temp_data = set()
-		for row in data:
-			if(len(row) > 0):
-				temp_data.add(row[0])
-		bad_words["./bad_word/csv_file/" + file] = temp_data
-for file in os.listdir("./bad_word/txt_file/"):
-	with open("./bad_word/txt_file/" + file, errors = 'ignore') as f:
-		temp_data = set()
-		for row in f:
-			if(len(row.strip()) > 0):
-				temp_data.add(row.strip())
-		bad_words["./bad_word/txt_file/" + file] = temp_data
-for file in os.listdir("./bad_word/comma_seperated_txt_file/"):
-	with open("./bad_word/comma_seperated_txt_file/" + file, errors = 'ignore') as f:
-		temp_data = set()
-		for row in f:
-			if not (len(row.strip()) == 0 or row.strip().startswith("##")):
-				for word in row.strip().split(', '):
-					temp_data.add(word)
-		bad_words["./bad_word/comma_seperated_txt_file/" + file] = temp_data
-
 ps = PorterStemmer() #stemmer
-stopwords = set(stopwords.words('english')) #stopword
-englishwords = set(nltk.corpus.words.words())
 labels = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
 
 #collect train data
@@ -60,197 +31,40 @@ def negative_features(sent):
 	#tag_words = nltk.pos_tag(words) #add tag into each word
 	#add features
 	dic = {}
-	dic.update(num_word(sent))
-	dic.update(num_unique_word(sent))
-	dic.update(ration_unique(sent))
-	dic.update(num_token_no_stop(words))
-	dic.update(num_spelling_error(words))
-	dic.update(num_allcap(words))
-	dic.update(rate_allcap(sent,words))
-	dic.update(length_cmt(sent))
-	dic.update(num_cap_letter(sent))
-	dic.update(rate_cap_letter(sent))
-	dic.update(num_explan_mark(sent))
-	dic.update(rate_explan_mark(sent))
-	dic.update(num_quest_mark(sent))
-	dic.update(rate_quest_mark(sent))
-	dic.update(num_punc_mark(sent))
-	dic.update(num_mark_sym(sent))
-	dic.update(num_smile(words))
-	dic.update(rate_lower(sent))
+	dic.update(utilities.num_word(sent))
+	dic.update(utilities.num_unique_word(sent))
+	dic.update(utilities.ration_unique(sent))
+	dic.update(utilities.num_token_no_stop(words))
+	dic.update(utilities.num_spelling_error(words))
+	dic.update(utilities.num_allcap(words))
+	dic.update(utilities.rate_allcap(sent,words))
+	dic.update(utilities.length_cmt(sent))
+	dic.update(utilities.num_cap_letter(sent))
+	dic.update(utilities.rate_cap_letter(sent))
+	dic.update(utilities.num_explan_mark(sent))
+	dic.update(utilities.rate_explan_mark(sent))
+	dic.update(utilities.num_quest_mark(sent))
+	dic.update(utilities.rate_quest_mark(sent))
+	dic.update(utilities.num_punc_mark(sent))
+	dic.update(utilities.num_mark_sym(sent))
+	dic.update(utilities.num_smile(words))
+	dic.update(utilities.rate_lower(sent))
+	dic.update(utilities.x20(words))
+	dic.update(utilities.x21(words))
+	dic.update(utilities.x22(words))
+	dic.update(utilities.x23(words))
+	dic.update(utilities.x24(words))
+	dic.update(utilities.x25(words))
 	return dic
-#features
-def num_word(raw):
-    '''
-    count the number of word (duplicated)
-    a word is defined as a substring seperated by space
-    input: raw : string
-    out put: dict num word: number of word 
-    '''
-    return ({'num_word': len(raw.split())})
-def num_unique_word(raw):
-    '''
-    count number of unique word (not duplicated)
-    input raw: string
-    output: dict num_unique_word: number of unique word
-    '''
-    return ({'num_unique_word':len(set(raw.split()))})
-def ration_unique(raw):
-    '''
-    compute ration of unique word
-    a word is a substring seperated by space
-    input raw : str
-    output: dict ration_unique : ration of unique word
-    '''
-    rate = num_unique_word(raw)['num_unique_word']/num_word(raw)['num_word']
-    return ({'ration unique': round(rate,3)})
-def num_token_no_stop(tokened):
-    '''
-    return number of token without stop word (depend on token function, can conclude '.', ',')
-    duplicated
-    intput tokened list : list
-    output dict num token no stop: number of tokens without stop word 
-    '''
-    no_stop= [w for w in tokened if w.lower() not in stopwords]
-    return ({'num_token_no_stop':len(no_stop)})
-def num_spelling_error(tokened):
-    '''
-    return number of word not in English vocab
-    can be counted differences, women, measures (hàm kém vl)
-    can be counted ',','.', 
-    input tokened: list
-    output: dict num spelling error :number of tokens not in English vocab
-    '''
-    spell_wrong= [w for w in tokened if w.lower() not in englishwords]
-    return ({'num_spelling_error':len(spell_wrong)})
-def num_allcap(tokened):
-    '''
-    return number of word written all captial (duplicated)
-    input tokened: list
-    output: dict num all cap: number of tokens written all capital
-    '''
-    cap= [w for w in tokened if w.isupper()]
-    return ({'num_all_cap':len(cap)})
-def rate_allcap(raw, tokened):
-    '''
-    return portion of word written all capital (duplicated)
-    input: raw: str
-           tokened: list
-    output: dict rate all cap :rate of all capital word
-    '''
-    rate = num_allcap(tokened)['num_all_cap']/ num_word(raw)['num_word']
-    return ({'rate_all_cap': round(rate, 3)})
-def length_cmt(raw):
-    '''
-    return length of the cmt
-    intput raw: str
-    output: dict 'length cmt':length of the cmt
-    '''
-    return ({'length_cmt': len(raw)})
-def num_cap_letter(raw):
-    '''
-    return number of capital letter
-    input raw: str
-    output: dict 'num cap letter':number of capital letter
-    '''
-    cap = [w for w in raw if w.isupper()]
-    return({'num_cap_letter':len(cap)})
-def rate_cap_letter(raw):
-    '''
-    return ratio of capital letter
-    input raw: str
-    outout: dict 'rate cap letter': ratio of capital letter
-    '''
-    rate = num_cap_letter(raw)['num_cap_letter']/len(raw)
-    return({'rate_cap_letter':round(rate, 3)})
-def num_explan_mark(raw):
-    '''
-    return number of explanation mark (not necessary using as explanation)
-    input raw: str
-    output: dict 'num explan mark': number of explanation mark
-    '''
-    count=0
-    for c in raw:
-        if c=='!':
-            count = count+1
-    return {'num_explan_mark':count}
-def rate_explan_mark(raw):
-    '''
-    return rate of explanation mark (not necessary using as explanation)
-    input raw: str
-    output: rate of explanation mark
-    '''
-    rate = num_explan_mark(raw)['num_explan_mark']/len(raw)
-    return({'rate_explan_mark':round(rate, 3)})
-def num_quest_mark(raw):
-    '''
-    return number of question mark (not necessary using as question)
-    input raw: str
-    output: dict 'num quest mark' number of question mark
-    '''
-    count=0
-    for c in raw:
-        if c=='?':
-            count=count+1
-    return {'num_quest_mark' : count}
-def rate_quest_mark(raw):
-    '''
-    return rate of question mark (not necessary using as question)
-    input raw: str
-    output: dict 'rate quest mark':rate of question mark
-    '''
-    rate = num_quest_mark(raw)['num_quest_mark']/len(raw)
-    return {'rate quest mark' : round(rate, 3)}
-def num_punc_mark(raw):
-    '''
-    return number of punctuation mark (not necessary using as finish setences)
-    input tokened: str
-    output: dict 'num punc mark': number of punctuation mark
-    '''
-    count=0
-    for c in raw:
-        if c=='.':
-            count=count+1
-    return {'num_ounc_mark': count}
-def num_mark_sym(raw):
-    '''
-    return number of marking symbol (*, &,$,%)
-    input raw: str
-    output: dict 'num mark sym' :number of marking symbol mark
-    '''
-    count=0
-    for c in raw:
-        if c in {'*','&','$','%'}:
-            count=count+1
-    return {'num_mark_sym': count}
-def num_smile(tokened):
-    '''
-    Count the number of emoji (can not count the case hoang:) or :))))
-    input: tokened: list
-    output:dict 'num smile': number of smile
-    '''
-    count= 0
-    for w in bigrams(tokened):
-        if w== (':',')'):
-            count=count+1
-    return {'num_smile':count}
-def rate_lower(raw):
-    '''
-    Count the rate of lowercase character
-    input raw: str
-    output: dict 'rate lower': rate of lowercase letter
-    '''
-    l = [w for w in raw if w.islower()]
-    rate = len(l)/len(raw)
-    return({'rate_lower': round(rate, 3)})
 
 #classify data using NaiveBayes
-feature_vector = [negative_features(sent) for (sent,tag) in train_data]
+feature_vector = [negative_features(sent) for (sent,tag) in train_data[:5000]]
 for label in labels:
 	featuresets = []
-	for i in range(len(train_data)):
+	for i in range(len(feature_vector)):
 		featuresets.append((feature_vector[i], train_data[i][1][label]))
 	size = int(0.1*len(featuresets))
+	size  = 2500
 	train_set, test_set = featuresets[size:], featuresets[:size]
 	classifier = nltk.NaiveBayesClassifier.train(train_set)
 
@@ -271,4 +85,4 @@ for label in labels:
 	recall = round(errorNN/(errorNN + errorNP),4)
 
 	print("Label {}: Accuracy = {} Precision = {} Recall = {}".format(label, nltk.classify.accuracy(classifier, test_set), precision, recall))
-	#classifier.show_most_informative_features(20)
+	#classifier.show_most_informative_features(50)
--- utilities.py
+++ utilities.py
@@ -1,148 +1,265 @@
+import csv
+import os
+import sys
+import nltk
+from nltk.corpus import stopwords
+from nltk.util import bigrams
 
-# for x20
-#cs.cmu
-# input: list of word, output: integer
-def x20(sent):
-    from urllib import request
-    url = 'https://www.cs.cmu.edu/~biglou/resources/bad-words.txt'
-    response = request.urlopen(url)
-    raw = response.read().decode('utf8')
-    # print(raw)  # anal, anus
-    list_bad_words = raw.split('\n')[1:]
+stopwords = set(stopwords.words('english')) #stopword
+englishwords = set(nltk.corpus.words.words())
+bad_words = {}
+
+for file in os.listdir("./bad_word/csv_file/"):
+    with open("./bad_word/csv_file/" + file, errors = 'ignore') as f:
+        data = csv.reader(f, delimiter = ";")
+        temp_data = set()
+        for row in data:
+            if(len(row) > 0):
+                temp_data.add(row[0])
+        bad_words[file] = temp_data
+for file in os.listdir("./bad_word/txt_file/"):
+    with open("./bad_word/txt_file/" + file, errors = 'ignore') as f:
+        temp_data = set()
+        for row in f:
+            if(len(row.strip()) > 0):
+                temp_data.add(row.strip())
+        bad_words[file] = temp_data
+for file in os.listdir("./bad_word/comma_seperated_txt_file/"):
+    with open("./bad_word/comma_seperated_txt_file/" + file, errors = 'ignore') as f:
+        temp_data = set()
+        for row in f:
+            if not (len(row.strip()) == 0 or row.strip().startswith("##")):
+                for word in row.strip().split(', '):
+                    temp_data.add(word)
+        bad_words[file] = temp_data
+
+def num_word(raw):
+    '''
+    count the number of word (duplicated)
+    a word is defined as a substring seperated by space
+    input: raw : string
+    out put: dict num word: number of word 
+    '''
+    return ({'num_word': len(raw.split())})
+def num_unique_word(raw):
+    '''
+    count number of unique word (not duplicated)
+    input raw: string
+    output: dict num_unique_word: number of unique word
+    '''
+    return ({'num_unique_word':len(set(raw.split()))})
+def ration_unique(raw):
+    '''
+    compute ration of unique word
+    a word is a substring seperated by space
+    input raw : str
+    output: dict ration_unique : ration of unique word
+    '''
+    rate = num_unique_word(raw)['num_unique_word']/num_word(raw)['num_word']
+    return ({'ration unique': round(rate,3)})
+def num_token_no_stop(tokened):
+    '''
+    return number of token without stop word (depend on token function, can conclude '.', ',')
+    duplicated
+    intput tokened list : list
+    output dict num token no stop: number of tokens without stop word 
+    '''
+    no_stop= [w for w in tokened if w.lower() not in stopwords]
+    return ({'num_token_no_stop':len(no_stop)})
+def num_spelling_error(tokened):
+    '''
+    return number of word not in English vocab
+    can be counted differences, women, measures (hàm kém vl)
+    can be counted ',','.', 
+    input tokened: list
+    output: dict num spelling error :number of tokens not in English vocab
+    '''
+    spell_wrong= [w for w in tokened if w.lower() not in englishwords]
+    return ({'num_spelling_error':len(spell_wrong)})
+def num_allcap(tokened):
+    '''
+    return number of word written all captial (duplicated)
+    input tokened: list
+    output: dict num all cap: number of tokens written all capital
+    '''
+    cap= [w for w in tokened if w.isupper()]
+    return ({'num_all_cap':len(cap)})
+def rate_allcap(raw, tokened):
+    '''
+    return portion of word written all capital (duplicated)
+    input: raw: str
+           tokened: list
+    output: dict rate all cap :rate of all capital word
+    '''
+    rate = num_allcap(tokened)['num_all_cap']/ num_word(raw)['num_word']
+    return ({'rate_all_cap': round(rate, 3)})
+def length_cmt(raw):
+    '''
+    return length of the cmt
+    intput raw: str
+    output: dict 'length cmt':length of the cmt
+    '''
+    return ({'length_cmt': len(raw)})
+def num_cap_letter(raw):
+    '''
+    return number of capital letter
+    input raw: str
+    output: dict 'num cap letter':number of capital letter
+    '''
+    cap = [w for w in raw if w.isupper()]
+    return({'num_cap_letter':len(cap)})
+def rate_cap_letter(raw):
+    '''
+    return ratio of capital letter
+    input raw: str
+    outout: dict 'rate cap letter': ratio of capital letter
+    '''
+    rate = num_cap_letter(raw)['num_cap_letter']/len(raw)
+    return({'rate_cap_letter':round(rate, 3)})
+def num_explan_mark(raw):
+    '''
+    return number of explanation mark (not necessary using as explanation)
+    input raw: str
+    output: dict 'num explan mark': number of explanation mark
+    '''
     count=0
-    for i in range(len(sent)):
-        if sent[i] in list_bad_words:
-            count+=1
-    return count
-# input: list of word, output: integer
-def x21(sent):
-    from urllib import request
-    url = 'http://www.bannedwordlist.com/lists/swearWords.txt'
-    response = request.urlopen(url)
-    raw = response.read().decode('utf8')
-    # print(raw)  # anal, anus
-    list_bad_words = raw.split('\r\n')
+    for c in raw:
+        if c=='!':
+            count = count+1
+    return {'num_explan_mark':count}
+def rate_explan_mark(raw):
+    '''
+    return rate of explanation mark (not necessary using as explanation)
+    input raw: str
+    output: rate of explanation mark
+    '''
+    rate = num_explan_mark(raw)['num_explan_mark']/len(raw)
+    return({'rate_explan_mark':round(rate, 3)})
+def num_quest_mark(raw):
+    '''
+    return number of question mark (not necessary using as question)
+    input raw: str
+    output: dict 'num quest mark' number of question mark
+    '''
     count=0
-    for i in range(len(sent)):
-        if sent[i] in list_bad_words:
-            count+=1
-    return count
-#input: the path to the file, this is a string
-def read_local_file(path):   #  for x22
-    file1=open(path,'r')
-    a= file1.read()
-    return a
-# input: list of word, output: integer
-def x22(sent):
-    raw_facebook = read_local_file('facebook-bad-words-list_comma-separated-text-file_2018_07_29.txt')
-    facebook_bad_words_list1 = raw_facebook.split(', ')
-    facebook_bad_words_list2 = [w for w in facebook_bad_words_list1 if ' ' not in w]
+    for c in raw:
+        if c=='?':
+            count=count+1
+    return {'num_quest_mark' : count}
+def rate_quest_mark(raw):
+    '''
+    return rate of question mark (not necessary using as question)
+    input raw: str
+    output: dict 'rate quest mark':rate of question mark
+    '''
+    rate = num_quest_mark(raw)['num_quest_mark']/len(raw)
+    return {'rate quest mark' : round(rate, 3)}
+def num_punc_mark(raw):
+    '''
+    return number of punctuation mark (not necessary using as finish setences)
+    input tokened: str
+    output: dict 'num punc mark': number of punctuation mark
+    '''
     count=0
-    for w in sent:
-        if w in facebook_bad_words_list2:
+    for c in raw:
+        if c=='.':
+            count=count+1
+    return {'num_ounc_mark': count}
+def num_mark_sym(raw):
+    '''
+    return number of marking symbol (*, &,$,%)
+    input raw: str
+    output: dict 'num mark sym' :number of marking symbol mark
+    '''
+    count=0
+    for c in raw:
+        if c in {'*','&','$','%'}:
+            count=count+1
+    return {'num_mark_sym': count}
+def num_smile(tokened):
+    '''
+    Count the number of emoji (can not count the case hoang:) or :))))
+    input: tokened: list
+    output:dict 'num smile': number of smile
+    '''
+    count= 0
+    for w in bigrams(tokened):
+        if w== (':',')'):
+            count=count+1
+    return {'num_smile':count}
+def rate_lower(raw):
+    '''
+    Count the rate of lowercase character
+    input raw: str
+    output: dict 'rate lower': rate of lowercase letter
+    '''
+    l = [w for w in raw if w.islower()]
+    rate = len(l)/len(raw)
+    return({'rate_lower': round(rate, 3)})
+# input: list of word, output: integer
+def x20(tokened):
+    count=0
+    dic = {}
+    for w in tokened:
+        if w in bad_words['bad-words.txt']:
             count+=1
-    return count
+            #dic["contains_{}".format(w)] = True
+    dic["bad_words_type_1"] = count
+    return (dic)
+# input: list of word, output: integer
+def x21(tokened):
+    count=0
+    dic = {}
+    for w in tokened:
+        if w in bad_words['swearWords.txt']:
+            count+=1
+            #dic["contains_{}".format(w)] = True
+    dic["bad_words_type_2"] = count
+    return (dic)
+# input: list of word, output: integer
+def x22(tokened):
+    count=0
+    dic = {}
+    for w in tokened:
+        if w in bad_words['facebook_bad_words.txt']:
+            count+=1
+            #dic["contains_{}".format(w)] = True
+    dic["bad_words_type_3"] = count
+    return (dic)
 
 # input: list of words, output: integer
-def x23(sent):
-    raw_youtube = read_local_file('facebook-bad-words-list_comma-separated-text-file_2018_07_29.txt')
-    youtube_bad_words_list1 = raw_youtube.split(', ')
-    youtbe_bad_words_list2 = [w for w in youtube_bad_words_list1 if ' ' not in w]
+def x23(tokened):
     count=0
-    for w in sent:
-        if w in youtbe_bad_words_list2:
+    dic = {}
+    for w in tokened:
+        if w in bad_words['youtube_bad_words.txt']:
             count+=1
-    return count
+            #dic["contains_{}".format(w)] = True
+    dic["bad_words_type_4"] = count
+    return (dic)
 
 #input: list of words, output:integer
-def x24(sent):
-    from urllib import request
-    url = 'https://gist.githubusercontent.com/ryanlewis/a37739d710ccdb4b406d/raw/0fbd315eb2900bb736609ea894b9bde8217b991a/google_twunter_lol'
-    response = request.urlopen(url)
-    raw = response.read().decode('utf8')
-    list_bad_words=raw.split('\n')
+def x24(tokened):
     count=0
-    for w in sent:
-        if w in list_bad_words:
+    dic = {}
+    for w in tokened:
+        if w in bad_words['google_twunter_lol.txt']:
             count+=1
-    return count
+            #dic["contains_{}".format(w)] = True
+    dic["bad_words_type_5"] = count
+    return (dic)
 
 #input: list of words, output:integer
-def x25(sent):
-    from urllib import request
-    url_cmu_20= 'https://www.cs.cmu.edu/~biglou/resources/bad-words.txt'
-    url_swear_21 = 'http://www.bannedwordlist.com/lists/swearWords.txt'
-    url_naugty_24 = 'https://gist.githubusercontent.com/ryanlewis/a37739d710ccdb4b406d/raw/0fbd315eb2900bb736609ea894b9bde8217b991a/google_twunter_lol'
-
-    response_20 = request.urlopen(url_cmu_20)
-    raw_20 = response_20.read().decode('utf8')
-    # print(raw)  # anal, anus
-    list_bad_words_20 = raw_20.split('\n')[1:]
-
-    response_21 = request.urlopen(url_swear_21)
-    raw = response_21.read().decode('utf8')
-    # print(raw)  # anal, anus
-    list_bad_words_21 = raw.split('\r\n')
-
-    raw_facebook_22 = read_local_file('facebook-bad-words-list_comma-separated-text-file_2018_07_29.txt')
-    facebook_bad_words_list1 = raw_facebook_22.split(', ')
-    list_bad_words_22 = [w for w in facebook_bad_words_list1 if ' ' not in w]
-
-    raw_youtube_23 = read_local_file('facebook-bad-words-list_comma-separated-text-file_2018_07_29.txt')
-    youtube_bad_words_list1 = raw_youtube_23.split(', ')
-    list_bad_words_23 = [w for w in youtube_bad_words_list1 if ' ' not in w]
-
-    response_24 = request.urlopen(url_naugty_24)
-    raw_24 = response_24.read().decode('utf8')
-    list_bad_words_24 = raw_24.split('\n')
-
-    total_bad_words= list_bad_words_20+list_bad_words_21+list_bad_words_22+list_bad_words_23+list_bad_words_24
-    distinct_bad_words=list(set(total_bad_words))
-
+def x25(tokened):
+    distinct_bad_words = set()
+    dic = {}
+    for key in bad_words.keys():
+        distinct_bad_words = distinct_bad_words.union(bad_words[key])
     count=0
-    for w in sent:
+    dic = {}
+    for w in tokened:
         if w in distinct_bad_words:
             count+=1
-    return count
-
-# input: nothing, output: the list of all bad words from the urls in x20,x21,x22,x23,x24
-def list_bad_words():
-    from urllib import request
-    url_cmu_20 = 'https://www.cs.cmu.edu/~biglou/resources/bad-words.txt'
-    url_swear_21 = 'http://www.bannedwordlist.com/lists/swearWords.txt'
-    url_naugty_24 = 'https://gist.githubusercontent.com/ryanlewis/a37739d710ccdb4b406d/raw/0fbd315eb2900bb736609ea894b9bde8217b991a/google_twunter_lol'
-
-    response_20 = request.urlopen(url_cmu_20)
-    raw_20 = response_20.read().decode('utf8')
-    # print(raw)  # anal, anus
-    list_bad_words_20 = raw_20.split('\n')[1:]
-
-    response_21 = request.urlopen(url_swear_21)
-    raw = response_21.read().decode('utf8')
-    # print(raw)  # anal, anus
-    list_bad_words_21 = raw.split('\r\n')
-
-    raw_youtube_23 = read_local_file('facebook_bad_words.txt')
-    youtube_bad_words_list1 = raw_youtube_23.split(', ')
-    list_bad_words_23 = [w for w in youtube_bad_words_list1 if ' ' not in w]
-
-    response_24 = request.urlopen(url_naugty_24)
-    raw_24 = response_24.read().decode('utf8')
-    list_bad_words_24 = raw_24.split('\n')
-
-    total_bad_words = list_bad_words_20 + list_bad_words_21 + list_bad_words_23 + list_bad_words_24
-    distinct_bad_words = list(set(total_bad_words))
-    return distinct_bad_words
-bad_words=list_bad_words()
-print(bad_words)
-print(len(bad_words))
-
-
-
-
-
-
-
-
-
-
+            #dic["contains_{}".format(w)] = True
+    dic["bad_words_all_type"] = count
+    return (dic)
