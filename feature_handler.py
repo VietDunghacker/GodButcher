688c6fa305c49eb61ae7b435cff71e9e362b3e43
Dunghacker <trungdunghoang92@gmail.com>
2020-05-11 15:49:07 +0700

    Update 1.0.1

--- feature_handler.py
+++ feature_handler.py
@@ -13,17 +13,18 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk.util import bigrams
 
+initialized = False
+up_to_date = True
+
 ps = PorterStemmer() #stemmer
 labels = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
-list_features = ["toxic","severe_toxic","obscene","threat","insult","identity_hate", "num_word","num_unique_word", "rate_unique", "num_token_no_stop", "num_spelling_error", "num_all_cap", "rate_all_cap", "length_cmt", "num_cap_letter", "rate_cap_letter", "num_explan_mark", "rate_explan_mark","num_quest_mark", "rate_quest_mark","num_punc_mark","num_mark_sym","num_smile","rate_space","rate_lower","bad_words_type_1","bad_words_type_2","bad_words_type_3","bad_words_type_4","bad_words_type_5","bad_words_all_type", "sentimental_score"]
+list_normal_features = ["num_word","num_unique_word", "rate_unique", "num_token_no_stop", "num_spelling_error", "num_all_cap", "rate_all_cap", "length_cmt", "num_cap_letter", "rate_cap_letter", "num_explan_mark", "rate_explan_mark","num_quest_mark", "rate_quest_mark","num_punc_mark","num_mark_sym","num_smile","rate_space","rate_lower","bad_words_type_1","bad_words_type_2","bad_words_type_3","bad_words_type_4","bad_words_type_5","bad_words_all_type"]
+list_sentiment_features = ["sentimental_score"]
 list_dependency_features = ["dependency_proper_noun_singular", "dependency_proper_noun_plural", "dependency_personal_pronoun", "dependency_possessive_pronoun", "dependency_with_denial", "dependency_denial_contain_proper_noun_singular", "dependency_denial_contain_proper_noun_plural", "dependency_denial_contain_personal_pronoun", "dependency_denial_contain_possessive_pronoun", "dependency_proper_noun_singular_and_denial", "dependency_proper_noun_plural_and_denial", "dependency_personal_pronoun_and_denial", "dependency_possessive_pronoun_and_denial", "dependency_contain_bad_words", "dependency_denial_contain_bad_words", "dependency_proper_noun_singular_bad_words", "dependency_proper_noun_plural_bad_words", "dependency_personal_pronoun_bad_words", "dependency_possessive_pronoun_bad_words", "dependency_pronoun_bad_words"]
+list_features = list_normal_features + list_dependency_features
 #collect train data
 collected_data = []
-with open("./toxic_comment/train.csv") as f:
-	data = csv.reader(f)
-	next(data)
-	for row in data:
-		collected_data.append((row[0],row[1],{"toxic":int(row[2]),"severe_toxic":int(row[3]),"obscene":int(row[4]),"threat":int(row[5]),"insult":int(row[6]), "identity_hate":int(row[7])}))
+
 
 def negative_features(sent):
 	words = word_tokenize(sent) #tokenize into list of words
@@ -57,16 +58,36 @@ def negative_features(sent):
 	dic.update(utilities.x24(words))
 	dic.update(utilities.x25(words))
 	dic.update(utilities.stm_score(sent))
-	#dic.update(utilities.dependency_features(sent))
+	dic.update(utilities.dependency_features(sent))
 	return dic
-output_data = []
-with open('./features.csv', mode = 'w') as output:
-	writer = csv.writer(output, delimiter = ',')
-	writer.writerow(['Comment'] + list_features)
-	for (idnum,comment, tags) in collected_data:
-		result_dict = negative_features(comment)
-		result_dict.update(tags)
-		result_list = []
-		for feature in list_features:
-			result_list.append(result_dict[feature])
-		writer.writerow([idnum] + result_list)
+
+#initialize the data
+def initialize():
+	if initialized == False:
+		open_test_data()
+		with open('./new_features.csv', mode = 'w') as output:
+			writer = csv.writer(output, delimiter = ',')
+			writer.writerow(['Comment'] + labels + list_features)
+			for (idnum,comment, tags) in collected_data[:100]:
+				result_dict = negative_features(comment)
+				result_dict.update(tags)
+				result_list = []
+				for label in labels:
+					result_list.append(result_dict[label])
+				for feature in list_features:
+					result_list.append(result_dict[feature])
+				writer.writerow([idnum] + result_list)
+
+#open the file containing train data
+def open_test_data():
+	with open("./toxic_comment/train.csv", encoding = 'utf-8') as f:
+		data = csv.reader(f)
+		next(data)
+		for row in data:
+			collected_data.append((row[0],row[1],{"toxic":int(row[2]),"severe_toxic":int(row[3]),"obscene":int(row[4]),"threat":int(row[5]),"insult":int(row[6]), "identity_hate":int(row[7])}))
+
+#update new features
+start = time.time()
+initialize()
+end = time.time()
+print(end-start)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import utilities
 import time
 import pandas as pd
 import random
+import feature_handler.py
 from urllib import request
 from nltk import FreqDist
 from nltk.stem import PorterStemmer 
@@ -19,11 +20,15 @@ ps = PorterStemmer() #stemmer
 labels = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
 
 list_features = ["num_word","num_unique_word", "rate_unique", "num_token_no_stop", "num_spelling_error", "num_all_cap", "rate_all_cap", "length_cmt", "num_cap_letter", "rate_cap_letter", "num_explan_mark", "rate_explan_mark","num_quest_mark", "rate_quest_mark","num_punc_mark","num_mark_sym","num_smile","rate_space","rate_lower","bad_words_type_1","bad_words_type_2","bad_words_type_3","bad_words_type_4","bad_words_type_5","bad_words_all_type", "sentimental_score"]
-start = time.time()
+#start = time.time()
+
+#read features already collected
 feature_data = pd.read_csv('./features.csv', header = 0)
-collected_data = []
-clean_data = []
-toxic_data = []
+collected_data = [] #a tuple(dictionary of features, tuple of six labels)
+clean_data = [] #data not labeled
+toxic_data = [] #data labled at least one
+
+#collect data into collected_data
 for i in range(len(feature_data)):
 	dic = {}
 	tags = []
@@ -35,6 +40,7 @@ for i in range(len(feature_data)):
 	collected_data.append((dic,tuple(tags)))
 #print("Collect data time is {}".format(time.time() - start))
 
+#seperate collected_data into clean_data and toxic_data
 for (features, tags) in collected_data:
 	if sum(tags) == 0:
 		clean_data.append((features,tags))
@@ -42,25 +48,33 @@ for (features, tags) in collected_data:
 		toxic_data.append((features,tags))
 random.shuffle(clean_data)
 random.shuffle(toxic_data)
-train_data = toxic_data[int(0.1 * len(toxic_data)):]
-test_data = toxic_data[:int(0.1 * len(toxic_data))]
-train_data = train_data + clean_data[int(0.1 * len(clean_data)):]
-test_data = test_data + clean_data[:int(0.1 * len(clean_data))]
 
+#select train data and test data
+train_data = toxic_data[int(0.25 * len(toxic_data)):]
+test_data = toxic_data[:int(0.25 * len(toxic_data))]
+train_data = train_data + clean_data[int(0.25 * len(clean_data)):]
+test_data = test_data + clean_data[:int(0.25 * len(clean_data))]
+
+'''extract the feature into feature vectors and then scale the feature.
+Each feature now has one of these three values: high, normal or medium, based on the normal distribution of 99% confidence interval'''
 feature_vector_train_data = [feature for (feature, tag) in train_data]
 feature_vector_test_data = [feature for (feature, tag) in test_data]
 feature_vector_train_data = utilities.feature_scaling(feature_vector_train_data)
 feature_vector_test_data = utilities.feature_scaling(feature_vector_test_data)
 
-classifiers = [None] * 6
+classifiers = [None] * 6 #contain 6 classifiers for each label
 for label in range(6):
 	train_set, test_set = [], []
 	for i in range(len(train_data)):
 		train_set.append((feature_vector_train_data[i], train_data[i][1][label]))
 	for i in range(len(test_data)):
 		test_set.append((feature_vector_test_data[i], test_data[i][1][label]))
-	classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+	classifier = nltk.NaiveBayesClassifier.train(train_set) #choose Naive Bayes classification
+	#classifier = nltk.DecisionTreeClassifier.train(train_set) #choose Decision Tree classification
 	classifiers[label] = classifier
+
+	#calculate precision and recall
 	errorPP, errorPN, errorNP, errorNN = 0, 0, 0, 0
 	for (feature, tag) in test_set:
 		predict = classifier.classify(feature)
@@ -84,7 +98,8 @@ for label in range(6):
 		recall = round(errorPP/(errorPP + errorNP),4)
 
 	print("Label {}: Accuracy = {} Precision = {} Recall = {}".format(labels[label], round(nltk.classify.accuracy(classifier, test_set),4), precision, recall))
-	#classifier.show_most_informative_features(30)
-	#print(classifier.pseudocode(depth = 20))
-end  = time.time()
-print("total time is {}".format(end - start))
+	#classifier.show_most_informative_features(30) #print most informative features in Naive Bayes classification
+	#print(classifier.pseudocode(depth = 20)) #print the pseudocode of Decision Tree classification
+
+'''end  = time.time()
+print("total time is {}".format(end - start))'''
